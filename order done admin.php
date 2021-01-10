bf2011d8272a66c948e61b130bf0fb71df909859
nabil <74468059+Nur178@users.noreply.github.com>
2021-01-10 15:20:04 +0600

    Update order done admin.php

--- order done admin.php
+++ order done admin.php
@@ -1,18 +1,19 @@
 <?php
-$db_host = 'localhost:3307'; // Server Name
-$db_user = 'root'; // Username
-$db_pass = ''; // Password
-$db_name = 'groceryshop'; // Database Name
+$db_host = "localhost:3307"; /* Server Name*/
+$db_user = 'root'; /* Username*/
+$db_pass = ''; /* Password*/
+$db_name = 'groceryshop'; /* Database Name*/
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-if (!$conn) {
+if (!$conn) 
+{
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());	
 }
 session_start();
-if ( isset( $_SESSION['USER_VALUE'] ) ) {
-    //echo("USER_ID present\n");
-	$USER_ID =$_SESSION['USER_VALUE'];
-	//echo $USER_ID;
+if ( isset( $_SESSION['USER_VALUE'] ) ) 
+{
+	$user_id = $_SESSION['USER_VALUE'];
+	
 }
 
 $sql = "(SELECT D.DONE_ORDER_NUMBER,D.PROVIDER_USER_ID, D.CUSTOMER_USER_ID ,D.PRODUCT, D.GIVEN_ORDER_DATE
@@ -21,7 +22,8 @@ $sql = "(SELECT D.DONE_ORDER_NUMBER,D.PROVIDER_USER_ID, D.CUSTOMER_USER_ID ,D.PR
 $query = mysqli_query($conn, $sql);
 
 
-if (!$query) {
+if (!$query) 
+{
 	die ('SQL Error: ' . mysqli_error($conn));
 }
 
@@ -31,14 +33,14 @@ if (!$query) {
 <html>
 <head>
 	<title>DISPLAYING RUNNING ORDER</title>
-	<link rel="stylesheet" type="text/css" href="styleTable.css">
+	<link rel = "stylesheet" type = "text/css" href = "styleTable.css">
 	
 </head>
 
-<body bgcolor="#E9F3F8">
+<body bgcolor = "#E9F3F8">
 <h1>Table 1</h1>
-	<table class="data-table">
-		<caption class="title">MY DONE ORDER </caption
+	<table class = "data-table">
+		<caption class = "title">MY DONE ORDER </caption
 		<thead>
 			<tr>
 			    <th>No</th>
@@ -52,11 +54,10 @@ if (!$query) {
 		</thead>
 		<tbody>
 		<?php
-		$no 	= 1;
-		$total 	= 0;
+		$no = 1;
+		$total = 0;
 		while ($row = mysqli_fetch_array($query))
 		{
-			
 			echo '<tr>
 					<td>'.$no.'</td>
 					<td>'.$row['DONE_ORDER_NUMBER'].'</td>
@@ -68,7 +69,8 @@ if (!$query) {
 					
 				</tr>';
 			$no++;
-		}?>
+		}
+		?>
 		</tbody>
 	</table>
 		
