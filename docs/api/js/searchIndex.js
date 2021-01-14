5f00f243e3d2105bb9f0d8873cc9dfa325d9a93d
Nur178 <nur.rahman@northsouth.edu>
2021-01-14 14:09:49 +0600

    Documentation done with all features

---
+++ docs/api/js/searchIndex.js
@@ -0,0 +1,24 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\REG\u0028\u0029",
+            "name": "REG",
+            "summary": "",
+            "url": "namespaces/default.html#function_REG"
+        },                {
+            "fqsen": "\\valueinfo",
+            "name": "valueinfo",
+            "summary": "",
+            "url": "classes/valueinfo.html"
+        },                {
+            "fqsen": "\\valueinfo\u003A\u003AtestCanBeCreatedFromValidReturnValue3\u0028\u0029",
+            "name": "testCanBeCreatedFromValidReturnValue3",
+            "summary": "",
+            "url": "classes/valueinfo.html#method_testCanBeCreatedFromValidReturnValue3"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
