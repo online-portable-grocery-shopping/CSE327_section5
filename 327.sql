1476e33175460486ecc1bfd5bb6085ea104edee5
sayem mahmud <sayem.mahmud97@gmail.com>
2021-01-15 16:03:31 +0600

    final testing with documentation and all features

--- 327.sql
+++ 327.sql
@@ -41,7 +41,7 @@ CREATE TABLE ADMINVIEW (
   )ENGINE=InnoDB DEFAULT CHARSET=latin1; 
   
  INSERT INTO ADMINVIEW (USERTYPE,FIRSTNAME,LASTNAME,EMAIL,USERNAME,PASSWORD,  RETYPEPASSWORD, GENDER, CONTACT ,LOCATION, NIDNUMBER,GROCERYNAME,TIME_FOR_GIVING_ORDER ,TIME_OF_REG) VALUES
- ('provider','sayem', 'mahmud', 'sayem.mahmud97@gmail.com', 'sayem', '@@@@@@@@@@@sam', '@@@@@@@@@@@sam','MALE' , '01626909723', 'Bashundhara','345','nila','sat-sun','19-11-2020'),
+ ('provider','sayem', 'mahmud', 'sayem.mahmud@gmail.com', 'sayem', '@@@@@@@@@@@sam', '@@@@@@@@@@@sam','MALE' , '01626909723', 'Bashundhara','345','nila','sat-sun','19-11-2020'),
  ('customer','Farah', 'Tasnur', 'farah.t@gmail.com', 'Farah Tasnur', 'farah','farah','FEMALE','01736372345' , 'Norda','','','',''),
  ('provider','sayem', 'mahmud', 'sayem.mah@northsouth.edu', 'saymm', 'sayem', 'sayem','MALE' , '01626909723', 'Bashundhara','345','nila','sat-sun','19-11-2020');
 
@@ -64,6 +64,7 @@ INSERT INTO AUTH_USER (FIRSTNAME,LASTNAME,EMAIL,USERNAME,PASSWORD,  RETYPEPASSWO
  
  ('Nur', 'Rahman', 'nur.rahman@gmail.com', 'Nur', 'nur','nur','MALE','01736372345' , 'khilgaon'),
  ('tasfiq', 'mahmud', 'tasfiq.mahmud@gmail.com', 'tasfiq', 'tasfiq', 'tasfiq','MALE' , '01626909723', 'Bashundhara'),
+ ('sayem', 'mahmud', 'sayem.mahmud97@gmail.com', 'sayem', '@@@@@@@@@@@sam', '@@@@@@@@@@@sam','MALE' , '01626909723', 'Bashundhara'),
  ('hasib', 'ahmed', 'hasib.ahmed@gmail.com', 'hasib', 'hasib', 'hasib','MALE' , '01626909756', 'Badda'),
  ('Farah', 'Tasnur', 'farah.tasnur@gmail.com', 'Farah Tasnur', 'farah','farah','FEMALE','01736372345' , 'Bata signal');
  
@@ -81,7 +82,8 @@ INSERT INTO AUTH_USER (FIRSTNAME,LASTNAME,EMAIL,USERNAME,PASSWORD,  RETYPEPASSWO
   )ENGINE=InnoDB DEFAULT CHARSET=latin1; 
   
   INSERT INTO PROVIDER(USER_ID,NIDNUMBER, GROCERYNAME, PRODUCTLIST, TIME_FOR_GIVING_ORDER) VALUES
-  (2,'347','yoyo', 'alu-poratha', 'sat-mon');
+  (2,'347','yoyo', 'alu-poratha', 'sat-mon'),
+  (3,'345','nila','soap,chips','sat-sun');
 
 
   
@@ -97,8 +99,9 @@ INSERT INTO AUTH_USER (FIRSTNAME,LASTNAME,EMAIL,USERNAME,PASSWORD,  RETYPEPASSWO
   INSERT INTO USER_ROLE (USER_ID,ROLE) VALUES
   (1,'admin'),
   (2,'provider'),
-  (3,'customer'),
-  (4,'customer');
+  (3,'provider'),
+  (4,'customer'),
+  (5,'customer');
   
   
   CREATE TABLE ORDERLIST (
@@ -114,7 +117,7 @@ INSERT INTO AUTH_USER (FIRSTNAME,LASTNAME,EMAIL,USERNAME,PASSWORD,  RETYPEPASSWO
    )ENGINE=InnoDB DEFAULT CHARSET=latin1; 
 
    INSERT INTO ORDERLIST (PROVIDER_USER_ID, CUSTOMER_USER_ID, PRODUCT,GIVEN_ORDER_DATE) VALUES
-   (2, 3, 'alu-parata', '18-01-2021' );
+   (2, 5, 'alu-parata', '18-01-2021' );
    
    
    CREATE TABLE DONEORDER (
@@ -130,13 +133,10 @@ INSERT INTO AUTH_USER (FIRSTNAME,LASTNAME,EMAIL,USERNAME,PASSWORD,  RETYPEPASSWO
   )ENGINE=InnoDB DEFAULT CHARSET=latin1;
  
   INSERT INTO DONEORDER (DONE_ORDER_NUMBER,PROVIDER_USER_ID, CUSTOMER_USER_ID, PRODUCT,GIVEN_ORDER_DATE) VALUES
-   (2, 2, 3, 'aluvegetables', '18-01-2021' );
+   (2, 2, 5, 'aluvegetables', '17-01-2021' );
 
    COMMIT;
  
  /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
- 
-
